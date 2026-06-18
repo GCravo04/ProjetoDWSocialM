92518c8d912b7db1f4e0942eff6ec4c4199790a8
GCravo04 <gabicravo04@gmail.com>
2026-06-18 15:09:21 +0100

    Criação do layout temporario da homepage

--- MVVM-MVC/MVVM/MVVM/Pages/Home/Index.cshtml.cs
+++ MVVM-MVC/MVVM/MVVM/Pages/Home/Index.cshtml.cs
@@ -1,7 +1,47 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace MVVM.Pages.Home;
 
-public class IndexModel : PageModel;
+public class IndexModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public IndexModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IList<Post> Posts { get; set; }
+        = new List<Post>();
+
+    [BindProperty]
+    public Post NewPost { get; set; }
+        = new();
+
+    public async Task OnGetAsync()
+    {
+        Posts = await _context.Posts
+            .Include(p => p.User)
+            .OrderByDescending(p => p.CreatedAt)
+            .ToListAsync();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            await OnGetAsync();
+            return Page();
+        }
+
+        NewPost.CreatedAt = DateTime.UtcNow;
+
+        _context.Posts.Add(NewPost);
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToPage();
+    }
+}
--- MVVM-MVC/MVVM/MVVM/Program.cs
+++ MVVM-MVC/MVVM/MVVM/Program.cs
@@ -1,15 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Connection String
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection");
+
+// DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Identity
+builder.Services.AddDefaultIdentity<AppUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Razor Pages
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -17,9 +33,11 @@ app.UseHttpsRedirection();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
 app.MapRazorPages()
     .WithStaticAssets();
 
