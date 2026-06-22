9e400cb25e1ca5e15ef700e6388307b354696a69
GCravo04 <gabicravo04@gmail.com>
2026-06-22 15:32:01 +0100

    fix do merge

--- MVVM-MVC/MVVM/MVVM/Pages/Home/Index.cshtml.cs
+++ MVVM-MVC/MVVM/MVVM/Pages/Home/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using MVC.Models;
 
 namespace MVVM.Pages.Home;
 
